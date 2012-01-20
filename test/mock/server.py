e1afd7913dc999ff7bc9085148a48a0fb36b4074
Tom MacWright <tom@macwright.org>
2012-01-19 23:08:46 -0500

    Stub out a python-based test mocking server.

---
+++ test/mock/server.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+# Simple mocking test server for jit_datasource
+
+import BaseHTTPServer, json, re
+
+def serve_view(s):
+    s.wfile.write(json.dumps({}))
+
+def serve_tile(s):
+    s.wfile.write(json.dumps({ "type": "FeatureCollection" }))
+
+class AsteroidMock(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_HEAD(s):
+        s.send_response(200)
+        s.send_header("Content-type", "application/json")
+        s.end_headers()
+    def do_GET(s):
+        s.send_response(200)
+        s.send_header("Content-type", "application/json")
+        s.end_headers()
+        if s.path == "/view/osm.road.json":
+            serve_view(s)
+        if re.search("/view/osm.road/\d+/\d+/\d+.geojson", s.path):
+            serve_tile(s)
+        else:
+            s.wfile.write("");
+
+HandlerClass = AsteroidMock
+ServerClass = BaseHTTPServer.HTTPServer
+Protocol = "HTTP/1.0"
+
+server_address = ('127.0.0.1', 9000)
+
+HandlerClass.protocol_version = Protocol
+httpd = ServerClass(server_address, HandlerClass)
+
+sa = httpd.socket.getsockname()
+print "Serving HTTP on", sa[0], "port", sa[1], "..."
+httpd.serve_forever()
